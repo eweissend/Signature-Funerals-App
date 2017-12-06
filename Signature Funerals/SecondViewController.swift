17c46bebcf0bae643d4554fda9485d65dca26d8e
Eli E. Weissend <eeweissend@bv0171104.local>
2017-12-06 07:56:22 -0600

    Initial Commit

---
+++ Signature Funerals/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Signature Funerals
+//
+//  Created by Eli E. Weissend on 12/6/17.
+//  Copyright © 2017 Eli E. Weissend. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
